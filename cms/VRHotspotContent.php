c90b62b5fa063f932468749064a91d2b46aab414
Magdum shaikh <94826076+smagdum124@users.noreply.github.com>
2023-07-17 16:40:08 +0530

    Update VRHotspotContent.php

--- cms/VRHotspotContent.php
+++ cms/VRHotspotContent.php
@@ -90,6 +90,14 @@ if(isset($_GET['DelId']) && isset($_GET['HotspotType']))
         margin: 0px;
         font-family: 'UNIVERSFORKPMG-BOLD';
         src: url("font/UNIVERSFORKPMG-BOLD.TTF") format("truetype");
+	               /* Added by magdum 17-07-23 */
+            /* for background image */
+            background-image: url(./assets/CMS-BG.jpg);
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: 100% 100%;
+            background-color: black;
     }
 
 
