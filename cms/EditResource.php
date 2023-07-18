991c6092bfe6deefa47e66fcbf6b91a1bb56d5d7
Magdum shaikh <94826076+smagdum124@users.noreply.github.com>
2023-07-18 17:35:31 +0530

    Update BG theme EditResource.php

--- cms/EditResource.php
+++ cms/EditResource.php
@@ -100,6 +100,16 @@ include_once('jwt.php');
     cursor: pointer;
     transition: 0.1s;
 }
+	    body{
+		         /* Added by magdum 18-07-23 */
+            /* for background image */
+            background-image: url(./assets/CMS-BG.jpg);
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: 100% 100%;
+            background-color: black;
+	    }
     </style>
 </head>
 
