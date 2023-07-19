a89c51f9b859241887a14efd6c4bf5a96a1afeb7
treesa821 <132673063+treesa821@users.noreply.github.com>
2023-07-19 17:24:13 +0530

    Update ajaxpost.php

--- cms/ajaxpost.php
+++ cms/ajaxpost.php
@@ -3,7 +3,7 @@ $allData = $_POST['allData'];
 print_r($allData);
 $i = 1;
 foreach ($allData as $key => $value) {
-    $sql = "UPDATE kpmg SET display_order=".$i." WHERE id=".$value;
+    $sql = "UPDATE IdleStateKaleidoscope SET VideoOrder =".$i." WHERE id=".$value;
     $conn->query($sql);
     $i++;
 }
